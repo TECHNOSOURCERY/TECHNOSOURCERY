aefe5f9eb708b43624c01e80a13e4879b30007ec
nevzista <nezakuttb@gmail.com>
2024-03-09 22:44:58 +0900

    restore-1

---
+++ kubejs/server_scripts/init_start_item.js
@@ -0,0 +1,11 @@
+// give items when player first log in
+// プレイヤーが最初にワールドに入った際のアイテム付与
+
+PlayerEvents.loggedIn((event) => {
+    if (!event.player.stages.has("starting_items")) {
+        event.player.stages.add("starting_items");
+        event.player.inventory.clear();
+        // set quest book
+        event.player.give("ftbquests:book");
+    }
+});
