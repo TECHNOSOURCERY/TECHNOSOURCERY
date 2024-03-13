dd5fc7b994caf4e7ba45d21a5b22291832f12a2d
crow-editing <umitaro556@gmail.com>
2024-03-13 20:22:04 +0900

    Added EMI hidden script for each mods

--- kubejs/server_scripts/recipes/ae2/ae2.js
+++ kubejs/server_scripts/recipes/ae2/ae2.js
@@ -1,7 +1,10 @@
 ServerEvents.recipes((events) => {
     // ME Item Cell Housing
     events.remove({ output: "ae2:item_cell_housing" });
-    events.shaped(Item.of("ae2:item_cell_housing"), ["ABA", "B B", "CCC"], {
+    events.shaped(Item.of("ae2:item_cell_housing"), 
+        ["ABA",
+         "B B",
+         "CCC"], {
         A: "ae2:quartz_glass",
         B: "immersiveengineering:wire_electrum",
         C: "mekanism:hdpe_sheet",
