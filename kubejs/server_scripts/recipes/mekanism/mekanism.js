67a1e5add61c229601a9d46cc2d533924af300e7
crow-editing <umitaro556@gmail.com>
2024-03-13 22:54:42 +0900

    mekanism factory

--- kubejs/server_scripts/recipes/mekanism/mekanism.js
+++ kubejs/server_scripts/recipes/mekanism/mekanism.js
@@ -7,4 +7,44 @@ ServerEvents.recipes((events) => {
     C: {tag: "forge:gears/invar"},
     D: "immersiveengineering:component_steel",
   })
+
+  // Metallurgic Infuser
+  events.remove({output: "mekanism:metallurgic_infuser"})
+  events.shaped(
+    Item.of("mekanism:metallurgic_infuser"),
+    ["CBC", "ADA", "CBC"],
+    {
+      A: {tag: "forge:gems/ruby"},
+      B: "ironfurnaces:iron_furnace",
+      C: {tag: "forge:ingots/osmium"},
+      D: "mekanism:steel_casing",
+    }
+  )
+})
+
+ServerEvents.recipes((events) => {
+  //remove basic factory recipes
+  events.remove({
+    output: [
+      "mekenism:basic_smelting_factory",
+      "mekenism:basic_enriching_factory",
+      "mekenism:basic_crushing_factory",
+    ],
+  })
+  events.remove({output: "mekenism:basic_compressing_factory"})
+  events.remove({output: "mekenism:basic_combining_factory"})
+  events.remove({output: "mekenism:basic_injecting_factory"})
+  events.remove({output: "mekenism:basic_purifying_factory"})
+  events.remove({output: "mekenism:basic_infusing_factory"})
+  events.remove({output: "mekenism:basic_sawing_factory"})
+  //remove advanced factory recipes
+  events.remove({output: "mekenism:advanced_smelting_factory"})
+  events.remove({output: "mekenism:advanced_enriching_factory"})
+  events.remove({output: "mekenism:advanced_crushing_factory"})
+  events.remove({output: "mekenism:advanced_compressing_factory"})
+  events.remove({output: "mekenism:advanced_combining_factory"})
+  events.remove({output: "mekenism:advanced_injecting_factory"})
+  events.remove({output: "mekenism:advanced_purifying_factory"})
+  events.remove({output: "mekenism:advanced_infusing_factory"})
+  events.remove({output: "mekenism:advanced_sawing_factory"})
 })
