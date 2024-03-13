56123fd2c592a73c3c2e298758ecb1beaf008f9f
CALL_OF_TAICHAN <162795763+moribayashitaiga@users.noreply.github.com>
2024-03-13 23:08:45 +0900

    Update tools.js

--- kubejs/server_scripts/recipes/minecraft/tools.js
+++ kubejs/server_scripts/recipes/minecraft/tools.js
@@ -1,8 +1,34 @@
 ServerEvents.recipes((events) => {
   // wooden pickaxe
   events.remove({output: "minecraft:wooden_pickaxe"})
-  events.shaped(Item.of("ae2:item_cell_housing"), ["AAA", " B ", " B "], {
-    A: "minecrafft:logs",
+  events.shaped(Item.of("minecraft:wooden_pickaxe"), ["AAA", " B ", " B "], {
+    A: "minecraft:logs",
     B: "forge:rods/wooden",
   })
 })
+
+  // iron pickaxe
+  events.remove({output: "minecraft:iron_pickaxe"})
+  events.shaped(Item.of("minecraft:iron_pickaxe"), ["AAA", " B ", " B "], {
+    A: "minecraft:iron_block",
+    B: "forge:rods/wooden",
+  })
+})
+
+  // wooden pickaxe
+  events.remove({output: "minecraft:iron_pickaxe"})
+  events.shaped(Item.of("minecraft:iron_pickaxe"), ["AAA", " B ", " B "], {
+    A: "minecraft:gold_block",
+    B: "forge:rods/wooden",
+  })
+})
+
+  // wooden pickaxe
+  events.remove({output: "minecraft:diamond_pickaxe"})
+  events.shaped(Item.of("minecraft:diamond_pickaxe"), ["ACA", " B ", " B "], {
+    A: "minecraft:logs",
+    B: "forge:rods/wooden"
+    C: "mekanism:enriched_diamond",
+  })
+})
+
