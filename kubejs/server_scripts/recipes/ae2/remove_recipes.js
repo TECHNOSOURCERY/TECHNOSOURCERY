e6d0286c0f33a20b3e4f87adf6ab8cdeddcf12ac
crow-editing <umitaro556@gmail.com>
2024-03-13 18:52:06 +0900

    added ignored file

---
+++ kubejs/server_scripts/recipes/ae2/remove_recipes.js
@@ -0,0 +1,4 @@
+ServerEvents.recipes(event => {
+    event.remove({ id: "mekanismadditions:pigment_extracting/plastic/reinforced/dark_red" })
+    event.remove({ id: 'mekanism:teleporter_frame' })
+})
