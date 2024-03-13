72d9f227f446569bf5fed5bce46358958ee3a3e5
CALL_OF_TAICHAN <162795763+moribayashitaiga@users.noreply.github.com>
2024-03-14 00:20:43 +0900

    Update tools.js

--- kubejs/server_scripts/recipes/minecraft/tools.js
+++ kubejs/server_scripts/recipes/minecraft/tools.js
@@ -3,8 +3,34 @@ ServerEvents.recipes((events) => {
   // wooden pickaxe
   events.remove({output: "minecraft:wooden_pickaxe"})
   events.shaped(Item.of("minecraft:wooden_pickaxe"), ["AAA", "BCB", "BCB"], {
-    A: "minecraft:netherite_block",
+    A: "minecraft:logs",
     B: "minecraft:air",
     C: "#forge:stick/wooden",
   })
+
+  // iron pickaxe
+  events.remove({output: "minecraft:iron_pickaxe"})
+  events.shaped(Item.of("minecraft:iron_pickaxe"), ["AAA", "BCB", "BCB"], {
+    A: "minecraft:iron_block",
+    B: "minecraft:air",
+    C: "#forge:stick/wooden",
+  })
+
+  // gold pickaxe
+  events.remove({output: "minecraft:gold_pickaxe"})
+  events.shaped(Item.of("minecraft:gold_pickaxe"), ["AAA", "BCB", "BCB"], {
+    A: "minecraft:gold_block",
+    B: "minecraft:air",
+    C: "#forge:stick/wooden",
+  })
+
+  // diamond pickaxe
+  events.remove({output: "minecraft:diamond_pickaxe"})
+  events.shaped(Item.of("minecraft:diamond_pickaxe"), ["AAA", "BCB", "BCB"], {
+    A: "minecraft:diamond_block",
+    B: "minecraft:air",
+    C: "#forge:stick/wooden",
+  })
+
+
 })
