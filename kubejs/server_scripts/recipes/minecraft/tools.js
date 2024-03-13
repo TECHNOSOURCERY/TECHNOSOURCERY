8d5f13e3d946b2762f7d377c5fe886d148ec1ea3
CALL_OF_TAICHAN <162795763+moribayashitaiga@users.noreply.github.com>
2024-03-14 00:18:05 +0900

    Update tools.js

--- kubejs/server_scripts/recipes/minecraft/tools.js
+++ kubejs/server_scripts/recipes/minecraft/tools.js
@@ -1,9 +1,9 @@
 ServerEvents.recipes((events) => {
   events.remove({output: ""})
-  // Machine Frame
+  // wooden pickaxe
   events.remove({output: "minecraft:wooden_pickaxe"})
   events.shaped(Item.of("minecraft:wooden_pickaxe"), ["AAA", "BCB", "BCB"], {
-    A: "minecraft:logs",
+    A: "minecraft:netherite_block",
     B: "minecraft:air",
     C: "#forge:stick/wooden",
   })
