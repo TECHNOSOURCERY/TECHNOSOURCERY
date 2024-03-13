c5ffeb312012f9ddac1f171ac1c33ee6fa27645e
crow-editing <umitaro556@gmail.com>
2024-03-13 12:44:58 +0900

    ワイルドカードを使用できるようにアップデート

--- deploy/Deploy_to_the_minecraft_servr.rb
+++ deploy/Deploy_to_the_minecraft_servr.rb
@@ -1,46 +1,69 @@
 require 'net/ftp'
 require 'yaml'
-def upload_file(ftp, local_filepath, remote_filepath)
-  file_size = File.size(local_filepath)
-  uploaded_size = 0
-  #進捗がわかるようMB単位でログを出力
-  File.open(local_filepath, 'rb') do |file|
-    ftp.storbinary("STOR #{remote_filepath}", file, Net::FTP::DEFAULT_BLOCKSIZE) do |data|
-      uploaded_size += data.size
-      progress_percent = (uploaded_size.to_f / file_size) * 100
-      remaining_size_mb = (file_size - uploaded_size) / 1024.0 / 1024.0
-      print "\rUploading #{File.basename(local_filepath)}: #{progress_percent.round(2)}% (Remaining: #{remaining_size_mb.round(2)}MB)"
-    end
+require 'logger'
+require 'pathname'
+# ログファイルの設定
+logger = Logger.new('ftp_upload.log', 'daily')
+logger.level = Logger::INFO
+# コンソールにも出力
+logger.formatter = proc do |severity, datetime, progname, msg|
+  puts "#{severity}: #{msg}" # コンソール出力フォーマットを設定
+  "#{severity}: #{msg}\n"  # ログファイル出力フォーマットを設定
+end
+# 完全なパスが除外リストに含まれるか判定するメソッド
+def excluded?(entry, base_dir, exclude_list)
+  exclude_list.any? do |pattern|
+    full_pattern = File.join(base_dir, pattern)
+    File.fnmatch?(full_pattern, entry, File::FNM_PATHNAME) ||
+    File.fnmatch?(File.join(full_pattern, '**', '*'), entry, File::FNM_PATHNAME)
   end
-  puts "" # 改行を追加
 end
-def excluded?(path, exclude_list)
-  exclude_list.any? {|excluded_item| path[/(^|\/)#{Regexp.escape(excluded_item)}(\/|$)/]}
+# ディレクトリがFTPサーバーに存在するかチェック
+def directory_exists?(ftp, dir)
+  ftp.chdir(dir)
+  true
+rescue Net::FTPPermError
+  false
 end
-
-def upload_directory(ftp, local_dir, remote_dir, exclude_list)
+# ファイルをアップロード
+def upload_file(ftp, local_filepath, remote_filepath, logger)
+  ftp.putbinaryfile(local_filepath, remote_filepath)
+  logger.info("Uploaded: #{local_filepath} -> #{remote_filepath}")
+end
+# ディレクトリをアップロード
+def upload_directory(ftp, local_dir, remote_dir, exclude_list, logger, base_dir)
   Dir.foreach(local_dir) do |entry|
     next if ['.', '..'].include?(entry)
     local_path = File.join(local_dir, entry)
     remote_path = File.join(remote_dir, entry)
+    # ファイルの完全なパスを取得
+    absolute_path = File.expand_path(local_path)
+    # ベースディレクトリからの相対パスを抽出
+    relative_path = Pathname.new(absolute_path).relative_path_from(Pathname.new(base_dir)).to_s
+    # 入力パスが除外リストに一致するかを確認
+    next if excluded?(absolute_path, base_dir, exclude_list)
     if File.directory?(local_path)
-      next if excluded?(local_path, exclude_list)
-      begin
-        puts "Creating directory : " + remote_path
+      unless directory_exists?(ftp, remote_path)
         ftp.mkdir(remote_path)
-      rescue Net::FTPPermError
-        # サーバー上にディレクトリがあればそのまま実行
+        logger.info("Created directory: #{remote_path}")
       end
-      upload_directory(ftp, local_path, remote_path, exclude_list)
+      upload_directory(ftp, local_path, remote_path, exclude_list, logger, base_dir)
     else
-      next if excluded?(local_path, exclude_list)
-      upload_file(ftp, local_path, remote_path)
+      upload_file(ftp, local_path, remote_path, logger)
     end
   end
 end
-# YAMLからコンフィグをロード
+# 設定ファイルを読み込む
 config = YAML.load_file('config.yml')
-Net::FTP.open(config['ftp']['host'], config['ftp']['user'], config['ftp']['password']) do |ftp|
+# ベースディレクトリを事前に取得
+base_dir = File.expand_path(config['local_dir_to_upload'])
+# ftp接続情報を取得
+ftp_config = config['ftp']
+# FTPに接続し、指定したディレクトリをアップロード
+Net::FTP.open(ftp_config['host'], ftp_config['user'], ftp_config['password']) do |ftp|
   ftp.passive = true
-  upload_directory(ftp, config['local_dir_to_upload'], config['remote_dir_to_upload_to'], config['exclude_list'])
+  logger.info("Started FTP upload to #{ftp_config['host']}")
+  upload_directory(ftp, base_dir, config['remote_dir_to_upload_to'], config['exclude_list'], logger, base_dir)
+  logger.info("FTP upload completed successfully.")
 end
+logger.close
